1677d1276b78b3bb3036b876f1495efb5bfd3c59
DimitrisPolitis-novi <dpolitis@novibet.com>
2024-08-04 10:09:46 +0300

    cleanup

---
+++ BlazorApp.Infrastructure/Configuration/ApplicationConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace BlazorApp.Infrastructure.Configuration
+{
+	public class ApplicationConfiguration
+	{
+		public BingoMapSettings BingoMapSettings { get; set; }
+		public ConnectionStrings ConnectionStrings { get; set; }
+		public ApplicationConfiguration()
+		{
+            BingoMapSettings = new BingoMapSettings();
+			ConnectionStrings = new ConnectionStrings();
+		}
+	}
+
+	public class BingoMapSettings
+    {
+		public BingoMapSettings()
+		{
+
+		}
+		public string BingoLocationsApiUrl { get; set; }
+		public string BingoLocationsApiCallTemplate { get; set; }
+		public string BingoLocationsApiKey { get; set; }
+		public string BingoMapLat { get; set; }
+		public string BingoMapLng { get; set; }
+
+	}
+
+	public class ConnectionStrings
+	{
+		public ConnectionStrings()
+		{
+
+		}
+		public string SQLiteDefaultConnection { get; set; }
+	}
+}
---
+++ BlazorApp.Infrastructure/Context/Context.cs
@@ -0,0 +1,36 @@
+﻿using BlazorApp.Infrastructure.Configuration;
+using BlazorApp.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorApp.Infrastructure.Context
+{
+	public class CustomerContext : DbContext
+	{
+		public DbSet<Customer> Customers { get; set; }
+
+		public static string DbPath;
+		static CustomerContext()
+		{
+		}
+		public CustomerContext(DbContextOptions<CustomerContext> options, IOptionsSnapshot<ApplicationConfiguration> applicationConfiguration) : base(options)
+		{
+
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+			var db = applicationConfiguration.Value.ConnectionStrings.SQLiteDefaultConnection;
+            DbPath = String.Concat("Data Source=", Path.Join(path, db));
+        }
+
+		// The following configures EF to create a Sqlite database file in the
+		// special "local" folder for your platform.
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			 options.UseSqlite(DbPath);
+		}
+	}
+
+}
